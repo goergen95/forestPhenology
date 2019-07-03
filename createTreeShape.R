50863a4582d41a4fc2ae93c84aa0f39711efd669
Eike Schott <eikeschott0@gmail.com>
2019-07-03 10:47:53 +0200

    copied lib loading functions to other scripts

--- createTreeShape.R
+++ createTreeShape.R
@@ -1,6 +1,9 @@
+loadandinstall = function(mypkg) {if (!is.element(mypkg, installed.packages()[,1])){install.packages(mypkg)};
+  library(mypkg, character.only = TRUE)}
+libs = c("rgdal","sp")
+lapply(libs,loadandinstall)
+
 #Converting .csv file to point shapefile
-library(rgdal)
-library(sp)
 trees = read.csv("data/trees_20190611.csv", sep = ";")
 crs = "+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 coords = trees[,2:3]
