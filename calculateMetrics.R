401f33cad20eb82826a8118b0264040f10d02d4f
goergen95 <darius2402@web.de>
2019-07-08 15:32:57 +0200

    fixed index loop

--- calculateMetrics.R
+++ calculateMetrics.R
@@ -10,18 +10,17 @@ lapply(libs,loadandinstall)
 trees = readOGR("data/artTrees.shp")
 files = list.files("data/resampled/",patter=".tif",full.names = TRUE)
 dates = readRDS("data/resampled/dates.rds")
-days = stringr::str_sub(dates[seq(1,16,3)],-15,-3)
+days = dates[seq(1,length(dates),3)]
 
 for (file in files){
   RGBseries = raster::brick(file)
   res = stringr::str_split(file,"/")[[1]][3]
   names(RGBseries) = dates
   steps = seq(1,nlayers(RGBseries),3)
-  days = unique(stringr::str_sub(dates,0,-3))
   for (step in steps){
-    date = stringr::str_sub(names(RGBseries[[seq(step,step+2,1)]])[1],-12,-3)
+    DOY = dates[step]
     indices = rgbIndices(RGBseries[[seq(step,step+2,1)]],rgbi=c("TGI","GLI","CIVE","IO","VVI","GCC","RCC"))
-    writeRaster(indices,filename=paste0("data/indices/indices_",days[which(steps==step)],"_",res),overwrite=TRUE)
+    writeRaster(indices,filename=paste0("data/indices/indices_",DOY,"_",res),overwrite=TRUE)
   }
 }
 
