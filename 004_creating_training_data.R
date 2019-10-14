ad3a5df1f5988cd642d314f929e3ca4b6adb6dc5
goergen95 <darius2402@web.de>
2019-10-14 13:42:57 +0200

    added evaluation of CNN prediction

--- 004_creating_training_data.R
+++ 004_creating_training_data.R
@@ -34,94 +34,52 @@ data = parallel::mclapply(treesLS, function(x){
   return(tmp)
 }, mc.cores = ncores)
 
+data2 = Matrix::
+
 n_pixel = dim(data[[1]])[1]
 n_vars = dim(data[[1]])[2]
+n_class = 2
+input_shape = n_pixel * n_vars
+res = sqrt(n_pixel)
 
 
-index = caret::createDataPartition(trees@data$specID, p = 0.2)
+index = caret::createDataPartition(trees@data$specID, p = 0.5)
 
 x_training = data[c(index$Resample1)]
 x_testing = data[-index$Resample1]
 K = keras::backend()
-x_train = lapply(x_training, unlist)
-x_train = unlist(x_train)
+x_train = unlist(x_training)
 x_train = matrix(x_train, nrow=length(index$Resample1))
+x_test = unlist(x_testing)
+x_test = matrix(x_test, nrow=length(trees$specID)-length(index$Resample1))
+
+x_train = scale(x_train)
+x_test = scale(x_test)
+
+
 
 y = keras::to_categorical(as.numeric(trees$specID)-1, 2)
 y_training = y[index$Resample1, ]
 y_testing = y[-index$Resample1, ]
 
 
-history = fit(model, x_train, y_training, batch_size = 10, epochs = 300)
+model = compileCNN(input_shape = input_shape,
+                  n_vars = n_vars,
+                  n_class = n_class,
+                  res = res)
 
+history = fit(model, x_train, y_training, batch_size = 10, epochs = 100)
+pred = predict(model, x_test)
 
+pred = lapply(1:nrow(pred), function(x){
+  which.max(pred[x,])
+})
+pred = unlist(pred)
+class = as.factor(c("BUR", "EIT"))
+pred = class[pred]
+obsv = trees$specID[-index$Resample1]
+
+caret::confusionMatrix(pred, obsv)
 
 data = as.data.frame(data)
 data = data[!is.na(data$z),]
-
-
-
-
-
-
-
-dummy = trees_buffer@data[,c("id","specID")]
-data$class = "NO"
-for (i in unique(dummy$id)){
-  ind = which(data$z == i)
-  data$class[ind] = as.character(dummy$specID[dummy$id == i][1])
-  print(i)
-}
-
-length(data$class[data$z == 4])
-
-
-model = keras::keras_model_sequential()
-model %>% layer_conv_2d(filters = 100,
-                        input_shape = c(16, 16, 139),
-                        kernel_size = 3,
-                        name = "block1_conv1") %>%
-  layer_activation_relu(name="block1_relu1") %>%
-  layer_conv_2d(filters = 80,
-                kernel_size = 3,
-                name = "block_1_conv2") %>%
-  layer_activation_relu(name="block1_relu2") %>%
-  #layer_max_pooling_2d(strides=2,
-   #                    pool_size = 5,
-   #                    name="block1_max_pool1") %>%
-  
-  layer_conv_2d(filters = 40,
-                kernel_size = 3,
-                name = "block2_conv1") %>%
-  layer_activation_relu(name="block2_relu1") %>%
-  layer_conv_2d(filters = 20,
-                kernel_size = 3,
-                name = "block2_conv2") %>%
-  layer_activation_relu(name="block2_relu2") %>%
-  # layer_max_pooling_2d(strides=2,
-  #                      pool_size = 5,
-  #                      name="block2_max_pool1") %>%
-  
-  # exit block
-  layer_global_max_pooling_2d(name="exit_max_pool") %>%
-  layer_dropout(rate=0.5) %>%
-  layer_dense(units = 2, activation = "softmax")
-
-compile(model, loss="categorical_crossentropy", optimizer="adam", metrics="accuracy")
-
-index = caret::createDataPartition(data$z, times = 1, p = 0.5)
-train = data[index$Resample1,]
-test = data[-index$Resample1,]
-
-
-K = keras::backend()
-x_train = as.matrix(data[,1:139])
-#x = K$expand_dims(x_train, axis = 2L)
-x_train = K$eval(x)
-data$class = as.factor(data$class)
-y_train = keras::to_categorical(as.numeric(data$class)-1, length(unique(data$class)))  
-
-
-history = fit(model, x_train, y_train, batch_size = 10, epochs = 300)
-
-                    
